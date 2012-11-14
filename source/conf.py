73a4f13305e1fa809cee19d85d25253b43e7876a
Riccardo Polignieri <ric.pol@libero.it>
2012-11-14 14:13:09 +0100

    added todolist

--- source/conf.py
+++ source/conf.py
@@ -86,6 +86,8 @@ pygments_style = 'sphinx'
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
 
+# todo list produces output:
+todo_include_todos = True
 
 # -- Options for HTML output ---------------------------------------------------
 
--- source/index.rst
+++ source/index.rst
@@ -14,6 +14,7 @@ Appunti wxPython
    documentarsi
    base_index
    intermedio_index
+   todolist
    
 Indices and tables
 ==================
